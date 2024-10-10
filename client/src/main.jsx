db2225140f95d7f4e7ccc5f7584226cedebaacaf
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-10-10 23:37:44 +0530

    added frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import store from "./store.js";
+import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App></App>}>
+      <Route index={true} path="/" element={<HomeScreen></HomeScreen>}></Route>
+      <Route path="/login" element={<LoginScreen></LoginScreen>}></Route>
+      <Route
+        path="/register"
+        element={<RegisterScreen></RegisterScreen>}
+      ></Route>
+
+      <Route path="" element={<PrivateRoute></PrivateRoute>}>
+        <Route
+          path="/profile"
+          element={<ProfileScreen></ProfileScreen>}
+        ></Route>
+      </Route>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router}></RouterProvider>
+    </StrictMode>
+  </Provider>
+);
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://147.79.68.221:5000/",
+        changeOrigin: true,
+      },
+    },
+  },
+});
