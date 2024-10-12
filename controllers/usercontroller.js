29cbc64f42e065a2d84b3f29d15036d6b398bdc8
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-10-13 01:41:02 +0530

    update crrct

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -114,8 +114,6 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("user not found");
   }
-
-  res.status(200).json({ message: "Update User Profile" });
 });
 
 export {
