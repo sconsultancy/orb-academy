f9f203381f021264a356f6feb81e9f27e5401d3d
Ashutosh2218 <66198054+Ashutosh2218@users.noreply.github.com>
2024-10-28 21:35:55 +0530

    server basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<>
+			<Button className=" bg-red-600">Check</Button>
+		</>
+	);
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+cors({
+	origin: process.env.CLIENT_URL,
+	methods: ["GET", "POST", "DELETE", "PUT"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+});
+
+app.use(express.json());
+
+// database connection
+mongoose
+	.connect(MONGO_URI)
+	.then(() => {
+		console.log("mongo connected");
+	})
+	.catch((e) => console.log(e));
+
+// Routes Configuration
 
 app.get("/", (req, res) => {
 	res.send({
@@ -8,6 +35,6 @@ app.get("/", (req, res) => {
 	});
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
 	console.log("server started");
 });
